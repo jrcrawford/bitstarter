b4d8f5be50eb3874b87cbe559893acf6f3f0d93d
Josh Crawford <josh@crawford.cc>
2013-07-05 20:28:29 -0400

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var indexBuf = fs.readFileSync("index.html");
+    var indexString = indexBuf.toString();
+    response.send(indexString);
 });
 
 var port = process.env.PORT || 5000;
