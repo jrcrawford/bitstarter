9a12c9b07ff9d45753e3ad6785363052a333ef26
Josh Crawford <josh@crawford.cc>
2013-07-17 17:37:34 -0400

    Styled Html

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var indexBuf = fs.readFileSync("index.html");
-    var indexString = indexBuf.toString();
-    response.send(indexString);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
